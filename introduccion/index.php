67a8f86a5ddb2e7ef72c3c8dde5abe8a4442993a
camiloruizvidal <camiloruizvidal@gmail.com>
2017-10-03 11:51:14 -0500

    #008

--- introduccion/index.php
+++ introduccion/index.php
@@ -7,7 +7,7 @@ $v='?v='.date('YmdHis');
 	  <title>Document</title>
 	  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	  <link rel="stylesheet" href="css/bootstrap.min.css">
-	  <link rel="stylesheet" href="css/estilos.css<?php echo $v;?>">
+	  <link rel="stylesheet" href="css/estilos.css<?php echo $v; ?>">
 		
 	</head>
 <body> 

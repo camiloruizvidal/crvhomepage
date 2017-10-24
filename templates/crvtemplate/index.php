7717958bf8abc45dd0335d31c37e64789ee063d5
camiloruizvidal <camiloruizvidal@gmail.com>
2017-10-24 16:03:48 -0500

    #017

--- templates/crvtemplate/index.php
+++ templates/crvtemplate/index.php
@@ -96,16 +96,10 @@
 							</div>
 							<div class="col-md-3">
 								<h3>ULTIMAS NOTICIAS</h3>
-								<ul>
-								<li><a href="/8-noticias/185-fiesta-de-aniversario">FIESTA DE ANIVERSARIO </a></li>
-							<li><a href="/8-noticias/184-11-aniversario-politecnico-latinoamericano-del-norte">11° ANIVERSARIO  POLITÉCNICO LATINOAMERICANO DEL NORTE</a></li>
-							<li><a href="/8-noticias/183-sala-de-simulacion-politecnico-latinoamericano-del-norte">SALA DE SIMULACIÓN POLITÉCNICO LATINOAMERICANO DEL NORTE</a></li>
-							<li><a href="/8-noticias/182-noticia-2">NOTICIA 1</a></li>
-							<li><a href="/8-noticias/180-delfin">delfin</a></li>
-								</ul>
+								<jdoc:include type="modules" name="position-noticiasrecientes" />
 							</div>
-							<div class="col-md-3">
-								<h3>CONTACTO</h3>
+					<div class="col-md-3">
+						<h3>CONTACTO</h3>
 								Direccion: <a href="https://www.google.com/maps/place/Politecnico+Latinoamericano+Del+Norte/@2.4421009,-76.6069539,17z/data=!4m5!3m4!1s0x8e30030f8e9a9279:0x5e6fe39fea57bb33!8m2!3d2.4421009!4d-76.6047652" target="_blank">Calle con carrera Popayán Cauca Colombia</a><br>
 								Telefono: <a href="tel:8369000">8369000</a><br>
 								Email: <a href="mailto:inf@solucionescrv.com">inf@solucionescrv.com</a>
@@ -114,18 +108,17 @@
 									$('#tel2').html('<a class="link" href="tel:Prueba de telefono"><i class="glyphicon glyphicon-phone-alt"></i> Prueba de telefono</a>');
 								});
 								</script>
-							</div>
-							<div class="col-md-3">
-								<h3>REDES SOCIALES</h3>
-								<a href="https://www.facebook.com/Politecnico-Latinoamericano-del-Norte-236525263194338/?fref=ts" target="_blank"><img src="http://politecnico.grupotriunfar.com/templates/crvtemplate/img/icons/fb.jpg" alt="Facebook" style="width: 40px;"></a><a href="https://www.youtube.com/watch?v=Wn3ANCGDbO0" target="_blank"><img src="http://politecnico.grupotriunfar.com/templates/crvtemplate/img/icons/youtube.png" alt="Youtube" style="width: 40px;"></a></div>
-
-														<div class="col-md-12">
-															<div id="copy-rigth">
-																<a href="http://solucionescrv.com" target="_blank">DISEÑO WEB POR <img src="<?php echo $tpath; ?>/img/icons/logo-crv.png" alt="logo crv"> SOLUCIONES SOFTWARE</a>
-															</div>
-														</div>
-													</div>
-												</footer>
-											</div>
+					</div>
+					<div class="col-md-3">
+						<h3>REDES SOCIALES</h3>
+						<a href="https://www.facebook.com/Politecnico-Latinoamericano-del-Norte-236525263194338/?fref=ts" target="_blank"><img src="http://politecnico.grupotriunfar.com/templates/crvtemplate/img/icons/fb.jpg" alt="Facebook" style="width: 40px;"></a><a href="https://www.youtube.com/watch?v=Wn3ANCGDbO0" target="_blank"><img src="http://politecnico.grupotriunfar.com/templates/crvtemplate/img/icons/youtube.png" alt="Youtube" style="width: 40px;"></a></div>
+					<div class="col-md-12">
+						<div id="copy-rigth">
+							<a href="http://solucionescrv.com" target="_blank">DISEÑO WEB POR <img src="<?php echo $tpath; ?>/img/icons/logo-crv.png" alt="logo crv"> SOLUCIONES SOFTWARE</a>
+						</div>
+					</div>
+				</div>
+			</footer>
+		</div>
 	</body>
 </html>

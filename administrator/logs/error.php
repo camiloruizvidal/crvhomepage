c5ceea404b9bc8359bf7e5851b61313990de7f49
JulianGarcia6 <32395783+JulianGarcia6@users.noreply.github.com>
2017-11-08 15:03:50 -0500

    #025

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2017-11-08 14:55:20 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2017-11-08T14:55:19+00:00	INFO ::1	joomlafailure	Clave vacía no permitida.
